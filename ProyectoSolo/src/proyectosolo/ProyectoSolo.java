eb22af5740ac6d1938e582a55d7807882a2e6dcb
a0y3770325h <a0y3770325h@s09-pc17>
2017-01-25 13:41:56 +0100

    he creado el proyecto

---
+++ ProyectoSolo/src/proyectosolo/ProyectoSolo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosolo;
+
+/**
+ *
+ * @author a0y3770325h
+ */
+public class ProyectoSolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       System.out.println("Creo el proyecto"); 
+    }
+    
+}
